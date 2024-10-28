e9394682bcd38dcc463011814d76aafd70752829
ria-l <55597349+ria-l@users.noreply.github.com>
2024-10-28 01:14:53 -0400

    display tests

--- binder_tests.js
+++ binder_tests.js
@@ -1,35 +1,10 @@
-// for each test, need to add a variation of this to the beginning:
-// setInputForCardSize('absolute', 80);
-// setInputsForGrid('absolute', 3, 3);
-// test_setup();
-// data = JSON.parse(localStorage.getItem('test_data'));
-// storeBinders(data)
-// createTags();
-// fillBinder(localStorage.getItem('bindername'));
-// populateDropdown();
-
-const test_setup = () => {
-  // dupe onLoad actions
-  localStorage.clear();
-  document.getElementById('status').innerHTML = '(test) loading...';
-  console.log('(test) fetching...');
-  console.log(`(test) fetched`);
-
-  localStorage.setItem('bindername', 'test');
-  localStorage.setItem(
-    'test_data',
-    `[["test_binder","file name","binder","name","caught","caught date","set","card type","pkmn type","card #","release date","dex #","pkmn type #","card type #"],["","sve.001.basic_g_energy.png","test_binder","basic_g_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","grass",1,"2023-03-31T04:00:00.000Z","",1,21],["","sve.002.basic_r_energy.png","test_binder","basic_r_energy","","2024-10-27T04:00:00.000Z","SVE","energy","fire",2,"2023-03-31T04:00:00.000Z","",2,21],["","sw.022.arcanine.jpg","test_binder","arcanine","x","2024-10-27T04:00:00.000Z","SW","energy","fire",22,"",59,2,21],["","lor.TG08.hisuian_arcanine.png","test_binder","arcanine","","2024-10-27T04:00:00.000Z","LOR","energy","fire",8,"2022-09-09T04:00:00.000Z",59,2,21],["","brs.TG01.flareon.png","test_binder","flareon","","2024-10-27T04:00:00.000Z","BRS","energy","fire",1,"2022-02-25T05:00:00.000Z",136,2,21],["","sve.003.basic_w_energy.png","test_binder","basic_w_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","water",3,"2023-03-31T04:00:00.000Z","",3,21],["","brs.TG02.vaporeon.png","test_binder","vaporeon","x","2024-10-27T04:00:00.000Z","BRS","energy","water",2,"2022-02-25T05:00:00.000Z",134,3,21],["","sve.004.basic_l_energy.png","test_binder","basic_l_energy","","2024-10-27T04:00:00.000Z","SVE","energy","lightning",4,"2023-03-31T04:00:00.000Z","",4,21],["","brs.TG04.jolteon.png","test_binder","jolteon","x","2024-10-27T04:00:00.000Z","BRS","energy","lightning",4,"2022-02-25T05:00:00.000Z",135,4,21],["","sve.005.basic_p_energy.png","test_binder","basic_p_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","psychic",5,"2023-03-31T04:00:00.000Z","",5,21],["","sve.006.basic_f_energy.png","test_binder","basic_f_energy","","2024-10-27T04:00:00.000Z","SVE","energy","fighting",6,"2023-03-31T04:00:00.000Z","",6,21],["","sve.007.basic_d_energy.png","test_binder","basic_d_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","dark",7,"2023-03-31T04:00:00.000Z","",7,21],["","sve.008.basic_m_energy.png","test_binder","basic_m_energy","","2024-10-27T04:00:00.000Z","SVE","energy","metal",8,"2023-03-31T04:00:00.000Z","",8,21]]`
-  );
-  console.log('setup done');
-};
-
 // createTags
 
 const subtest_create_tags_writes_to_storage = () => {
   createTags();
   tags = localStorage.getItem('tags');
   if (tags.includes('basic_g_energy')) {
-    console.log('tags written: pass');
+    console.log('pass');
   } else {
     console.log('tags written: fail');
     console.log(`tags:\n${tags}`);
@@ -41,7 +16,7 @@ const subtest_tags_width_matches_input = () => {
 
   width = document.getElementById('inputCardSize').value;
   if (tags.includes(`${width}px`)) {
-    console.log('correct width: pass');
+    console.log('pass');
   } else {
     console.log(`correct width: fail\n${width}px`);
   }
@@ -97,14 +72,14 @@ const test_storebinders = () => {
 
   const stored_binder = localStorage.getItem('test_binder');
   if (stored_binder) {
-    console.log('binder stored');
+    console.log('pass');
   } else {
-    console.log('binder not stored');
+    console.log('fail: binder not stored');
   }
   if (stored_binder.includes('arcanine')) {
-    console.log('binder data stored');
+    console.log('pass');
   } else {
-    console.log('binder data not stored');
+    console.log('fail: binder data not stored');
   }
 };
 
@@ -118,7 +93,6 @@ const test_fillbinder = () => {
   storeBinders(data);
   createTags();
 
-  const cardTags = localStorage.getItem('tags').split(',');
   const rows = parseInt(document.getElementById('inputRow').value);
   const cols = parseInt(document.getElementById('inputCol').value);
 
@@ -129,7 +103,7 @@ const test_fillbinder = () => {
   }
   content = document.getElementById('content').innerHTML;
   if (content.startsWith('<table') && content.endsWith('</table>')) {
-    console.log('correct table format');
+    console.log('pass');
   } else {
     console.log(`incorrect table format: ${content}`);
   }
@@ -137,7 +111,7 @@ const test_fillbinder = () => {
     content.includes('hisuian_arcanine') &&
     content.includes('basic_g_energy')
   ) {
-    console.log('correct contents');
+    console.log('pass');
   } else {
     console.log(`incorrect contents: ${content}`);
   }
@@ -146,14 +120,14 @@ const test_fillbinder = () => {
       '<tr><td><img src="img/sve/sve.001.basic_g_energy.png" title="sve.001.basic_g_energy.png : grass : energy" style="width:80px;"></td><td><img src="img/sword_shield_promos/sword_shield_promos.SWSH146.poke_ball.png" title="sve.002.basic_r_energy.png : fire : energy" style="width:80px;"></td><td><img src="img/sw/sw.022.arcanine.jpg" title="sw.022.arcanine.jpg : fire : energy" style="width:80px;"></td></tr>'
     )
   ) {
-    console.log('correct cols');
+    console.log('pass');
   } else {
     console.log(`incorrect cols. input cols: ${cols}`);
     console.log(content);
   }
 
   if (content.match(new RegExp('<tr>', 'g')).length == 5) {
-    console.log('correct rows');
+    console.log('pass');
   } else {
     console.log(
       `incorrect rows. input rows: ${rows}\ntr count: ${
---
+++ display_tests.js
@@ -0,0 +1,108 @@
+// setInputForCardSize
+
+const test_setInputForCardSize = () => {
+  try {
+    setInputForCardSize('absolute', 365);
+  } catch (err) {
+    console.log(err);
+  }
+  let w = document.getElementById('inputCardSize').value;
+  if (w == '365') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 365, ${w}`);
+  }
+
+  try {
+    setInputForCardSize('relative', -300);
+  } catch (err) {
+    console.log(err);
+  }
+  w = document.getElementById('inputCardSize').value;
+  if (w == '65') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 65, ${w}`);
+  }
+
+  try {
+    setInputForCardSize('relative', 100);
+  } catch (err) {
+    console.log(err);
+  }
+  w = document.getElementById('inputCardSize').value;
+  if (w == '165') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 165, ${w}`);
+  }
+};
+
+const subtest_grid_abs = () => {
+  try {
+    setInputsForGrid('absolute', 2, 4);
+  } catch (err) {
+    console.log(err);
+  }
+  let r = document.getElementById('inputRow').value;
+  let c = document.getElementById('inputCol').value;
+
+  if (r == '4') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 4, ${r}`);
+  }
+  if (c == '2') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 2, ${c}`);
+  }
+};
+
+const subtest_grid_minus = () => {
+  setInputsForGrid('absolute', 2, 4);
+  try {
+    setInputsForGrid('relative', -2, -1);
+  } catch (err) {
+    console.log(err);
+  }
+  c = document.getElementById('inputCol').value;
+  r = document.getElementById('inputRow').value;
+  if (c == '0') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 0, ${c}`);
+  }
+  if (r == '3') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 3, ${r}`);
+  }
+};
+
+const subtest_grid_plus = () => {
+  setInputsForGrid('absolute', 2, 4);
+  try {
+    setInputsForGrid('relative', 3, 7);
+  } catch (err) {
+    console.log(err);
+  }
+  c = document.getElementById('inputCol').value;
+  r = document.getElementById('inputRow').value;
+  if (c == '5') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 5, ${c}`);
+  }
+  if (r == '11') {
+    console.log('pass');
+  } else {
+    console.log(`fail: 11, ${r}`);
+  }
+};
+
+const test_setInputsForGrid = () => {
+  subtest_grid_abs();
+  subtest_grid_minus();
+  subtest_grid_plus();
+};
