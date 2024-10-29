0e100abf4a3eef064936a7007dfdca44c0398711
ria-l <55597349+ria-l@users.noreply.github.com>
2024-10-28 22:13:44 -0400

    moar test cards

--- js/tests_setup.js
+++ js/tests_setup.js
@@ -17,20 +17,18 @@ const test_setup = () => {
   localStorage.setItem(
     'test_data',
     `[["test_binder","file name","binder","name","caught","caught date","set","card type","pkmn type","card #","release date","dex #","pkmn type #","card type #"],
-    ["","sve.001.basic_g_energy.png","test_binder","basic_g_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","grass",1,"2023-03-31T04:00:00.000Z","",1,21],
-    ["","sve.002.basic_r_energy.png","test_binder","basic_r_energy","","2024-10-27T04:00:00.000Z","SVE","energy","fire",2,"2023-03-31T04:00:00.000Z","",2,21],
-    ["","sw.022.arcanine.jpg","test_binder","arcanine","x","2024-10-27T04:00:00.000Z","SW","energy","fire",22,"",59,2,0],
-    ["","lor.TG08.hisuian_arcanine.png","test_binder","arcanine","x","2024-10-27T04:00:00.000Z","LOR","energy","fighting",8,"2022-09-09T04:00:00.000Z",59,6,0],
-    ["","brs.TG01.flareon.png","test_binder","flareon","x","2024-10-27T04:00:00.000Z","BRS","energy","fire",1,"2022-02-25T05:00:00.000Z",136,2,0],
-    ["","sve.003.basic_w_energy.png","test_binder","basic_w_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","water",3,"2023-03-31T04:00:00.000Z","",3,21],
-    ["","brs.TG02.vaporeon.png","test_binder","vaporeon","x","2024-10-27T04:00:00.000Z","BRS","energy","water",2,"2022-02-25T05:00:00.000Z",134,3,0],
-    ["","sve.004.basic_l_energy.png","test_binder","basic_l_energy","","2024-10-27T04:00:00.000Z","SVE","energy","lightning",4,"2023-03-31T04:00:00.000Z","",4,21],
-    ["","brs.TG04.jolteon.png","test_binder","jolteon","x","2024-10-27T04:00:00.000Z","BRS","energy","lightning",4,"2022-02-25T05:00:00.000Z",135,4,0],
-    ["","sve.005.basic_p_energy.png","test_binder","basic_p_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","psychic",5,"2023-03-31T04:00:00.000Z","",5,21],
-    ["","sve.006.basic_f_energy.png","test_binder","basic_f_energy","","2024-10-27T04:00:00.000Z","SVE","energy","fighting",6,"2023-03-31T04:00:00.000Z","",6,21],
-    ["","sve.007.basic_d_energy.png","test_binder","basic_d_energy","x","2024-10-27T04:00:00.000Z","SVE","energy","dark",7,"2023-03-31T04:00:00.000Z","",7,21],
-    ["","sve.008.basic_m_energy.png","test_binder","basic_m_energy","","2024-10-27T04:00:00.000Z","SVE","energy","metal",8,"2023-03-31T04:00:00.000Z","",8,21],
-    ["","xyp.XY177.karen_.jpg","test_binder","karen_","x","2024-10-27T04:00:00.000Z","XYP","support","n/a",177,"2023-03-31T04:00:00.000Z","",19,20]]`
+      ["","sve.001.basic_g_energy.png","test_binder","basic_g_energy","x","2023-3-31T04:00:00.000Z","SVE","energy","grass",1,"2023-3-31T04:00:00.000Z","basic_g_energy",1,21],
+      ["","sve.002.basic_r_energy.png","test_binder","basic_r_energy","x","2023-3-31T04:00:00.000Z","SVE","energy","fire",2,"2023-3-31T04:00:00.000Z","basic_r_energy",2,21],
+      ["","sw.022.arcanine.jpg","test_binder","arcanine","","","SW","basic","fire",22,"2007-11-7T04:00:00.000Z",59,2,0],
+      ["","brs.TG01.flareon.png","test_binder","flareon","x","2022-2-25T04:00:00.000Z","BRS","basic","fire",1,"2022-2-25T04:00:00.000Z",136,2,0],
+      ["","sve.003.basic_w_energy.png","test_binder","basic_w_energy","","","SVE","energy","water",3,"2023-3-31T04:00:00.000Z","basic_w_energy",3,21],
+      ["","brs.TG02.vaporeon.png","test_binder","vaporeon","x","2022-2-25T04:00:00.000Z","BRS","basic","water",2,"2022-2-25T04:00:00.000Z",134,3,0],
+      ["","brs.TG04.jolteon.png","test_binder","jolteon","x","2022-2-25T04:00:00.000Z","BRS","basic","lightning",4,"2022-2-25T04:00:00.000Z",135,4,0],
+      ["","xyp.XY177.karen_.jpg","test_binder","karen_","x","2016-9-23T04:00:00.000Z","XYP","support","n/a",177,"2016-9-23T04:00:00.000Z","karen_",19,20],
+      ["","brs.TG22.umbreon_v.png","test_binder","umbreon","x","2022-2-25T04:00:00.000Z","BRS","v","dark",22,"2022-2-25T04:00:00.000Z",136.2,7,14],
+      ["","lor.TG17.pikachu_vmax.png","test_binder","pikachu","x","2022-9-9T04:00:00.000Z","LOR","vmax","lightning",17,"2022-9-9T04:00:00.000Z",25,4,15],
+      ["","crz.GG56.hisuian_zoroark_vstar.png","test_binder","zoroark","x","2023-1-20T04:00:00.000Z","CRZ","vstar","dark",56,"2023-1-20T04:00:00.000Z",571,7,16],
+      ["","lor.TG08.hisuian_arcanine.png","test_binder","arcanine","x","2022-9-9T04:00:00.000Z","LOR","basic","fighting",8,"2022-9-9T04:00:00.000Z",59,6,0]]`
   );
 };
 
