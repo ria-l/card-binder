eb1b72cfe6b068569644d3d6d374cdea41dbaf07
ria-l <55597349+ria-l@users.noreply.github.com>
2024-11-24 02:02:03 -0500

    refactor storage functions

--- js/store.js
+++ js/store.js
@@ -4,76 +4,55 @@ import * as sorting from './sorting.js';
 export function storeData(data) {
   const header = data[0];
   localStorage.setItem('header', header);
-  storeBinders(header, data);
-  storeSets(header, data);
+  const binderName = storeCardData(header, data, 'binder');
+  const setName = storeCardData(header, data, 'set');
+  storeFileNames('binder', binderName);
+  storeFileNames('set', setName);
 }
 
-function storeBinders(header, data) {
-  let bindername = localStorage.getItem('bindername');
-  if (!bindername) {
-    bindername = header[0]; // TODO: make this random
-    localStorage.setItem('bindername', bindername);
+function storeCardData(header, data, container) {
+  let containerName = localStorage.getItem(`${container}name`);
+  if (!containerName) {
+    if (container == 'binder') {
+      containerName = header[0]; // TODO: make this random
+    } else if (container == 'set') {
+      containerName = 'PAL'; // TODO: make this random
+    }
+    localStorage.setItem(`${container}name`, containerName);
   }
-  // store binder names
-  const binderNames = new Set();
-  const binderCol = header.indexOf('binder');
+  // store container names
+  const containerNames = new Set();
+  const containerCol = header.indexOf(container);
   for (const row of data) {
-    if (row[binderCol] != 'binder') {
-      binderNames.add(row[binderCol]);
+    if (row[containerCol] != container) {
+      containerNames.add(row[containerCol]);
     }
   }
-  localStorage.setItem('bindernames', JSON.stringify([...binderNames]));
-  // store data for each binder
-  for (const bName of binderNames) {
+  localStorage.setItem(
+    `${container}names`,
+    JSON.stringify([...containerNames])
+  );
+  // store data for each container
+  for (const name of containerNames) {
     // only the cards that are in the given binder
-    filterAndSort(data, binderCol, bName, header);
+    let filtered = data.filter((row) => row[containerCol] == name);
+    // add back the header, since it would be removed during filtering
+    filtered.unshift(header);
+    localStorage.setItem(name, JSON.stringify(sorting.sortByColor(filtered)));
   }
-  // store filenames for current binder
-  storeFileNames('binder', bindername);
+  return containerName;
 }
 
 /**
  *
- * @param {*} data
- * @param {int} col column number for set or binder
- * @param {string} bsName binder or set name
- * @param {array of strings} header from the gSheet
+ * @param {string} container binder or set
+ * @param {string} containerName name of binder or set
  */
-export function filterAndSort(data, col, bsName, header) {
-  let filtered = data.filter((row) => row[col] == bsName);
-  // add back the header, since it would be removed during filtering
-  filtered.unshift(header);
-  localStorage.setItem(bsName, JSON.stringify(sorting.sortByColor(filtered)));
-}
-
-function storeSets(header, data) {
-  let setname = localStorage.getItem('setname');
-  if (!setname) {
-    setname = 'PAL'; // TODO: make this random
-    localStorage.setItem('setname', setname);
-  }
-  // store set names
-  const setNames = new Set();
-  const setCol = header.indexOf('set');
-  for (const row of data) {
-    setNames.add(row[setCol]);
-  }
-  localStorage.setItem('setnames', JSON.stringify([...setNames]));
-  // store data for each binder
-  for (const sName of setNames) {
-    filterAndSort(data, setCol, sName, header);
-  }
-  // store filenames for current binder
-  storeFileNames('set', setname);
-}
-
-/**
- *
- * @param {string} type binder or set
- * @param {string} _name name of binder or set
- */
-export function storeFileNames(type, _name) {
-  const data = JSON.parse(localStorage.getItem(_name));
+export function storeFileNames(container, containerName) {
+  const data = JSON.parse(localStorage.getItem(containerName));
   const filenames = data.map((row) => row[constants.FILENAME_COL]);
-  localStorage.setItem(`${type}_filenames`, JSON.stringify([...filenames]));
+  localStorage.setItem(
+    `${container}_filenames`,
+    JSON.stringify([...filenames])
+  );
 }
